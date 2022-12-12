b63991a685147fdd3f6637cb2bee2d53cbdb1ac7
Lloyd Stevens <stevens.lloyd@gmail.com>
2022-12-12 09:36:12 +0000

    update make dog

--- problems/03-make-dog.js
+++ problems/03-make-dog.js
@@ -1,6 +1,37 @@
 // Your code here
+class Dog {
+	//constructor function should take in a name argument and 
+	//set the name property of a newly instantiated instance to the name argument
+	constructor(name){
+		this.name = name;
+	}
 
+	// 
+	static makeJet() {
+		return new Dog("Jet");
+	}
 
+	changeName(newName) {
+		this.name = newName;
+
+		return this.name;
+	}
+
+	speak(word){
+		return `${this.name} says ${word}`;
+	}
+}
+
+let dog1 = Dog.makeJet(); // returns an object
+
+console.log(dog1.name);                 // Jet
+console.log(dog1.speak("hello"));       // Jet says hello
+console.log(dog1.changeName("Freyja")); // Freyja
+console.log(dog1.name);                 // Freyja
+console.log(dog1.speak("hello"));       // Freyja says hello
+
+let dog2 = Dog.makeJet();
+console.log(dog2.name);                 // Jet
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
 
