94e5fbe7cc60aebbb821a1ad035a6e5b9d8a0331
Lloyd Stevens <stevens.lloyd@gmail.com>
2022-12-12 09:25:07 +0000

    update calculator

--- problems/02-calculator.js
+++ problems/02-calculator.js
@@ -1,5 +1,45 @@
 // Your code here
+class Calculator {
+	constructor() {
+		this.total=0;
+	}
 
+	add(num) {
+		this.total = this.total+num;
+	return this.total;
+
+	}
+
+	subtract(num) {
+		this.total = this.total-num;
+		return this.total;
+	
+		}
+
+	divide(num){
+		this.total = this.total/num;
+		return this.total;
+	}
+	
+	multiply (num){
+
+		this.total = this.total*num;
+	return this.total;
+
+	}
+
+
+
+
+
+}
+
+let calculator = new Calculator();
+console.log(calculator.add(50));      // => 50
+console.log(calculator.subtract(35)); // => 15
+console.log(calculator.multiply(10)); // => 150
+console.log(calculator.divide(5));    // => 30
+console.log(calculator.total)         // => 30
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
